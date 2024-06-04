13945f65c7dd5b1ab20a336ec94907ae742b9a4b
KartashovIvan <vano.noname69@yandex.ru>
2024-06-04 17:28:53 +0300

    Выполнен первый этап. Начато выполнение второго этапа

---
+++ src/main/java/org/javaacademy/afisha/servcie/InitDatabaseService.java
@@ -0,0 +1,25 @@
+package org.javaacademy.afisha.servcie;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.PathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.Objects;
+
+@Service
+//@Profile("first")
+@RequiredArgsConstructor
+public class InitDatabaseService {
+    private final JdbcTemplate jdbcTemplate;
+    private final static String sqlScript = "src/main/resources/sql/init.sql";
+
+    @PostConstruct
+    public void init() throws SQLException {
+        ScriptUtils.executeSqlScript(Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection(), new PathResource(sqlScript));
+    }
+}
